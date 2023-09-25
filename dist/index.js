a80b28f6bd290813a44190e9cd4fb13ceb072532
ibrahim2375 <ibrahim.abdelrahman.abdelaziz@gmail.com>
2023-09-25 18:58:22 +0300

    added

---
+++ dist/index.js
@@ -0,0 +1,20 @@
+"use strict";
+function returnNumber(value) {
+    return value;
+}
+function returnString(value) {
+    return value;
+}
+function returnBoolean(value) {
+    return value;
+}
+function returnValue(value) {
+    return value;
+}
+function returnType(value) {
+    return value;
+}
+console.log(returnType(100));
+console.log(returnType("hima"));
+console.log(returnType(false));
+//# sourceMappingURL=index.js.map
---
+++ dist/test.js
@@ -0,0 +1,3 @@
+"use strict";
+console.log(Math.floor(10.7));
+//# sourceMappingURL=test.js.map
